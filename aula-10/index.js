47d5f7f3948f4ab6a698b629f98b507349eef1b3
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-06-21 23:03:27 -0300

    Recursos Nativos do Navegador
    Utilizando o conceito de console(log, error e warn), window(prompt, alert) e document(write).

---
+++ aula-10/index.js
@@ -0,0 +1,14 @@
+let nome = 'Aluizio';
+
+console.log(nome); // nome impresso no console = Aluizio
+console.error(nome); // nome impresso no console em erro = Aluizio
+console.warn(nome); // nome impresso no console em aviso = Aluizio
+
+//let confirmar = window.confirm('Deseja realmente excluir?'); // confirm para excluir
+
+//window.prompt('Digite seu nome:'); // prompt para digitar o nome
+//window.alert(nome); // alerta com o nome Aluizio
+
+//console.log(confirmar); // confirmar impresso no console = true
+
+document.write(nome); // nome impresso na tela = Aluizio
