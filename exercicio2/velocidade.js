a695971ff216e71a3833e35ee248ca7d5411ccbd
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-06-23 23:22:03 -0300

    Update to velocidade.js

--- exercicio2/velocidade.js
+++ exercicio2/velocidade.js
@@ -6,21 +6,17 @@ mais rápido (ou que as velocidades são iguais se este for o caso)
 
 /* Inserindo nome e velocidade de dois veiculos */
 
-const veiculo1 = prompt("Digite o nome do primeiro veículo: ");
-const velocidade1 = parseFloat(prompt("Digite a velocidade do primeiro veículo: "));
+const veiculo1 = prompt("Informe o nome do primeiro veículo:")
+const velocidade1 = parseFloat(prompt("Informe a velocidade do primeiro veículo:"))
 
-const veiculo2 = prompt("Digite o nome do segundo veículo: ");
-const velocidade2 = parseFloat(prompt("Digite a velocidade do segundo veículo: "));
+const veiculo2 = prompt("Informe o nome do segundo veículo:")
+const velocidade2 = parseFloat(prompt("Informe a velocidade do segundo veículo:"))
 
 /* Verificando qual veículo é mais rápido e exibindo à tela */
-
-let message;
 if (velocidade1 > velocidade2) {
-    message = veiculo1 + " é mais rápido que " + veiculo2;
+    alert(veiculo1 + " é mais rápido do que " + veiculo2)
 } else if (velocidade2 > velocidade1) {
-    message = veiculo2 + " é mais rápido que " + veiculo1;
+    alert(veiculo2 + " é mais rápido do que " + veiculo1)
 } else {
-    message = veiculo1 + " e " + veiculo2 + " têm a mesma velocidade";
+    alert(veiculo1 + " e " + veiculo2 + " possuem velocidades iguais.")
 }
-
-alert(message);
