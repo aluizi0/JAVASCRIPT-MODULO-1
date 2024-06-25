6670532ccb7660cadff463c82af3e65a69a3260d
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-06-25 09:26:41 -0300

    While
    Estrutura de Repetição.

---
+++ aula-22/while.js
@@ -0,0 +1,11 @@
+let = velocidade = 80;
+
+while(velocidade > 0){
+    
+    alert("O carro está a " + velocidade + " km/h")
+    velocidade -= 20
+    alert("O carro está diminuindo " + velocidade + " km/h")
+
+}
+
+alert("O carro parou.")
