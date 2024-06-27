9fa87e5ac6e3c72443d14716cc75d6a4aad4bc01
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-06-27 09:40:33 -0300

    Laço de Repetição For
    Estrutura de repetição utilizada, contendo as seguintes ordens:
    (Inicialização; Condição; Finalização)
    Ex: for(let i=0; i <= 10; i++).

---
+++ aula-28/for.js
@@ -0,0 +1,11 @@
+/* Inicialização; Condição; Finalização */
+
+/*for(let indice = 0; indice <= 10; indice++) {
+  alert("indice = " + indice);
+}*/
+
+let nome="Aluizio";
+
+for(let indice = 0; indice < nome.length; indice++) {
+  console.log(nome[indice]);
+}
