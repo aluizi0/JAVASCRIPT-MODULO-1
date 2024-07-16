6f8fd32d7878f1452cfe67a98d389f66db37f2d4
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-07-16 10:34:19 -0300

    Exercicio 10(emprego.js)
    Finalizando o exercício.

--- exercicio10/emprego.js
+++ exercicio10/emprego.js
@@ -51,7 +51,7 @@ function inscreverCandidato(){
     const confirmacao = confirm(
         "Nome : " + vaga.nome +
         "\nDescrição: " + vaga.descricao +
-        "\nData limite: " + vaga.dataLimite +
+        "\nData limite: " + vaga.dataLimite
     )
 
     if(confirmacao){
@@ -67,7 +67,7 @@ function excluirVaga(){
     const confirmacao = confirm(
         "Nome : " + vaga.nome +
         "\nDescrição: " + vaga.descricao +
-        "\nData limite: " + vaga.dataLimite +
+        "\nData limite: " + vaga.dataLimite
     )
 
     if(confirmacao){
@@ -89,6 +89,38 @@ function exibirMenu(){
 
     return opcao;
 }
+
+function executar(){
+    let opcao;
+    do{
+        opcao = exibirMenu();
+
+        switch(opcao){
+            case "1":
+                listarVagas();
+                break;
+            case "2":
+                novaVaga();
+                break;
+            case "3":
+                exibirVaga();
+                break;
+            case "4":
+                inscreverCandidato();
+                break;
+            case "5":
+                excluirVaga();
+                break;
+            case "6":
+                alert("Saindo...");
+                break;
+            default:
+                alert("Opção inválida!");
+        }
+    }while(opcao !== "6");
+}
+
+executar();
 
 /*
     Sistema de Vagas de Emprego
